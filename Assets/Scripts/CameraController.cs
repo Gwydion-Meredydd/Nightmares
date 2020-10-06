12482d83934d25774931ddebd1981717842a0df0
Eruheto <49587148+Eruheto@users.noreply.github.com>
2020-10-06 19:19:06 +0100

    Enemy Spawning/Nav mesh
    I am creating a basic spawning script for me to then create a player follow navmesh

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -13,12 +13,6 @@ public class CameraController : MonoBehaviour
     public Vector3 maxPosition;
     public Vector3 minPosition;
 
-
-    void Start()
-    {
-
-    }
-
     void LateUpdate()
     {
         if (game_Manager.InGame == true)
@@ -26,21 +20,26 @@ public class CameraController : MonoBehaviour
             if (game_Manager.Paused == false)
             {
                 if (transform.position != player.position)
+                //is the camera in the same position as the player
                 {
                     Vector3 targetPosition = new Vector3(player.position.x,
                                                            yValue,
                                                            player.transform.position.z);
+                    //keep the x and z value the same as the player while holding the same position on the y
 
                     targetPosition.x = Mathf.Clamp(targetPosition.x,
                                                     minPosition.x,
                                                     maxPosition.x);
+                    //lock the camera x axis so it can't go outside the bounds of the level
                     targetPosition.z = Mathf.Clamp(targetPosition.z,
                                                   minPosition.z,
                                                   maxPosition.z);
+                    //lock the camera z axis so it can't go outside the bounds of the level
 
                     Camera.transform.position = Vector3.Lerp(Camera.position,
                                                       targetPosition,
                                                       smoothing);
+                    //creates slight lag in the camera follow movement to make it seem smoother
                 }
             }
         }
---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyScript : MonoBehaviour
+{
+    public Transform spawnLocationOne, spawnLocationTwo, spawnLocationThree, spawnLocationFour;
+    private GameObject player;
+    public GameObject enemy;
+    public int numberEnemies;
+    bool canSpawn = true;
+    public int waveDelay = 20;
+
+    void Update()
+    {
+        if (canSpawn == true)
+        {
+            if (GameObject.FindGameObjectsWithTag("Enemy").Length <= 0)
+            {
+                canSpawn = false;
+                StartCoroutine(spawning());                
+            }
+        }
+    }
+
+    IEnumerator spawning()
+    {
+        for (int i = 0; i < numberEnemies; i++)
+        {
+            int spawnChoice = Random.Range(1, 4);
+            switch (spawnChoice)
+            {
+                case 1:
+                    Instantiate(enemy, spawnLocationOne.position, Quaternion.identity);
+                    break;
+
+                case 2:
+                    Instantiate(enemy, spawnLocationTwo.position, Quaternion.identity);
+                    break;
+
+                case 3:
+                    Instantiate(enemy, spawnLocationThree.position, Quaternion.identity);
+                    break;
+
+                case 4:
+                    Instantiate(enemy, spawnLocationFour.position, Quaternion.identity);
+                    break;
+
+                default:
+                    Instantiate(enemy, spawnLocationFour.position, Quaternion.identity);
+                    break;
+            }
+        }
+
+        canSpawn = true;
+    }
+}
