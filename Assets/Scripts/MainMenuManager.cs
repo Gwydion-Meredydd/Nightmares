a402ec9606f4dfc3c0b3aa48b1935fd4c61bf791
ollyrowlands <49551669+ollyrowlands@users.noreply.github.com>
2020-11-22 17:50:04 +0000

    Main Menu
    Added some simple features that need finishing, as well as 75% completing the UI

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject mainMenuObj;
+    public GameObject optionsMenuObj;
+
+    public void StartGame()
+    {
+        Debug.Log("Starting Game");
+    }
+
+    public void OptionsButton()
+    {
+        mainMenuObj.SetActive(false);
+        optionsMenuObj.SetActive(true);
+    }
+
+    public void Leaderboards()
+    {
+        Debug.Log("Leaderboards Loading");
+    }
+
+    public void BackButton()
+    {
+        mainMenuObj.SetActive(true);
+        optionsMenuObj.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting");
+
+        Application.Quit();
+    }
+}
