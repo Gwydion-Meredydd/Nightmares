699808241ca42ddcdf50a46c0abdcf5a11e8b673
Gwydion-Meredydd <310137@gllm.ac.uk>
2021-01-20 16:24:03 +0000

    Some MOre stuff

---
+++ Assets/PauseMenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    public ScriptsManager SM;
+
+    public void Update()
+    {
+        if (SM.GameScript.InGame) 
+        {
+            if (Input.GetKeyDown(KeyCode.Escape)) 
+            {
+                PauseToggle();
+            }
+        }
+    }
+    public void PauseToggle() 
+    {
+        if (SM.GameScript.Paused) 
+        {
+            SM.GameScript.Paused = false;
+        }
+        else 
+        {
+            SM.GameScript.Paused = true;
+        }
+
+    }
+}
